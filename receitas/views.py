269869a1dc66966cf7cfd575aba30c0db3cee2b8
Bárbara <6abi.cardoso@gmail.com>
2021-06-20 01:27:01 -0300

    Nome dinâmico das receitas com dicionarios

--- receitas/views.py
+++ receitas/views.py
@@ -4,7 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de legumes',
+        3: 'Suco verde',
+        4: 'Sorvete',
+        5: 'Bolo de Limão',
+        6: 'Suco de abacaxi com hortelã'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+    return render(request,'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
