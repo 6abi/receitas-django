5b86317226015058459511e9b2eefe72a2e5a380
Bárbara <6abi.cardoso@gmail.com>
2021-06-22 19:59:32 -0300

    Cria o app usuarios e faz as rotas dos novos metodos

--- alurareceitas/settings.py
+++ alurareceitas/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'receitas',
     'pessoas',
+    'usuarios',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ usuarios/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def cadastro(request):
+    return
+
+def login(request):
+    return
+
+def dashboard(request):
+    return
+
+def logout(request):
+    return
