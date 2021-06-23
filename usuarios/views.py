932d61e6c5cdeca5025d1d6b3111fb3db05419d7
Bárbara <6abi.cardoso@gmail.com>
2021-06-22 21:20:46 -0300

    Criação de usuarios no banco de dados pelo forms

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,15 +1,47 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 # Create your views here.
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        
+        if not nome.strip():
+            print("O campo nome não pode ficar em branco")
+            return redirect('cadastro')
+        if not email.strip():
+            print("O campo email não pode ficar em branco")
+            return redirect('cadastro')
+        if senha != senha2:
+            print("As senhas não são iguais!")
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print("Usuário já cadastrado")
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print("Usuário cadastrado com sucesso")
+        return  redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            print("Os campos email e senha não podem ficar em branco")
+            return redirect('login')
+
+        return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 def dashboard(request):
-    return
+    return render(request, 'usuarios/dashboard.html')
 
 def logout(request):
     return
