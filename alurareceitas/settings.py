2238b4c9b58e8dc440e43ee5178a00d7fa8f32e3
Bárbara <6abi.cardoso@gmail.com>
2021-06-23 22:23:00 -0300

    Final parte 3. Codigo refactorado e criação da pasta de Apps

--- alurareceitas/settings.py
+++ alurareceitas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 from decouple import config
@@ -145,3 +146,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.SUCCESS: 'success',
 }
+
+##Apps location
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0,os.path.join(PROJECT_ROOT, '../apps'))
